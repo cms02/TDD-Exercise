7e148ad783161df21058881cb0080a6341ecf3a9
cms02 <minoui1234@gmail.com>
2023-01-19 18:02:13 +0900

    first commit

---
+++ src/test/java/com/cms/atdd/membership/MembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.atdd.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
